3662797a8d11bbc7e07886876411aaffd2daa871
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2019-04-03 17:17:32 +0530

    test: spec file customer.component

--- src/app/customers/customer.component.spec.ts
+++ src/app/customers/customer.component.spec.ts
@@ -1,25 +1,60 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerComponent } from './customer.component';
+import {FormBuilder} from '@angular/forms';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {getBlankCustomer} from './testing/test-customer';
 
-describe('CustomerComponent', () => {
-  let component: CustomerComponent;
-  let fixture: ComponentFixture<CustomerComponent>;
+////// Testing Vars //////
+let component: CustomerComponent;
+let fixture: ComponentFixture<CustomerComponent>;
+// create new instance of FormBuilder
+const formBuilder: FormBuilder = new FormBuilder();
 
+
+/*describe('CustomerComponent', () => {
   beforeEach(async(() => {
+    fixture = TestBed.createComponent(CustomerComponent);
+    component = fixture.componentInstance;
     TestBed.configureTestingModule({
       declarations: [ CustomerComponent ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should have emailMatcher method',  () =>  {
+    expect(component).toBeDefined();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+});*/
+
+describe('CustomerComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CustomerComponent],
+      providers: [
+        FormBuilder
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CustomerComponent);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  // create reusable function for a dry spec.
+  function updateForm(userEmail, userPassword) {
+    component.customerForm.controls['email'].setValue(userEmail);
+    component.customerForm.controls['password'].setValue(userPassword);
+  }
+  it('should have default props', () => {
+    expect(component.user).toEqual(getBlankCustomer());
   });
+
+/*  it('should have emailMatcher',  () => {
+    component
+  });*/
 });
